17897d9d841fb6dab8f3abff5e47599708fa48c7
roLLlyTHuK <ircvipua@gmail.com>
2024-01-25 22:51:28 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Particles, { initParticlesEngine } from '@tsparticles/react';
+import { loadFull } from 'tsparticles';
+import Home from './containers/home/index';
+import About from './containers/about/index';
+import Skills from './containers/skills/index';
+import Portfolio from './containers/portfolio/index';
+import Resume from './containers/resume/index';
+import Contact from './containers/contact/index';
+import NavBar from './components/navBar/index';
+import { particles } from './utils/particles';
+import { useEffect, useState } from 'react';
+import './App.scss';
 
 function App() {
+  const [init, setInit] = useState(false);
+  const location = useLocation();
+  const renderParticlesHome = location.pathname === '/';
+  useEffect(() => {
+    initParticlesEngine(async engine => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* particles.js */}
+      {renderParticlesHome && init && (
+        <Particles id="tsparticles" options={particles} init={init} />
+      )}
+
+      {/* <NavBar /> */}
+      <NavBar />
+      {/* main page content */}
+      <div className="App__main-page-content">
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/components/navBar/index.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { FaBars, FaReact } from 'react-icons/fa';
+import { HiX } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
+import './styles.scss';
+
+const data = [
+  {
+    label: 'Home',
+    to: '/',
+  },
+  {
+    label: 'About',
+    to: '/about',
+  },
+  {
+    label: 'Resume',
+    to: '/resume',
+  },
+  {
+    label: 'Skills',
+    to: '/skills',
+  },
+  {
+    label: 'Portfolio',
+    to: '/portfolio',
+  },
+  {
+    label: 'Contact',
+    to: '/contact',
+  },
+];
+
+const NavBar = () => {
+  const [toggleIcon, setToggleIcon] = useState(false);
+  const handlerToggleIcon = () => {
+    setToggleIcon(!toggleIcon);
+  };
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar__container">
+          <Link to={'/'} className="navbar__container__logo">
+            <FaReact size={30} />
+          </Link>
+
+          <ul
+            className={`navbar__container__menu ${toggleIcon ? 'active' : ''}`}
+          >
+            {data.map((item, key) => (
+              <li key={key} className="navbar__container__menu__item">
+                <Link
+                  to={item.to}
+                  className="navbar__container__menu__item__links"
+                >
+                  {item.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className="nav-icon" onClick={handlerToggleIcon}>
+            {toggleIcon ? <HiX size={30} /> : <FaBars size={30} />}
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
