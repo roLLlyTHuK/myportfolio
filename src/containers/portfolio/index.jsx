a3904921df05a94a59917d0ce493403003eac094
roLLlyTHuK <ircvipua@gmail.com>
2024-01-27 23:24:12 +0200

    update

--- src/containers/portfolio/index.jsx
+++ src/containers/portfolio/index.jsx
@@ -1,14 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageHeaderContent from '../../components/pageHeaderContent';
 import { BsBriefcaseFill } from 'react-icons/bs';
+import { portfolioData, filterData } from '../../utils/portfolioData';
+import './styles.scss';
 
 const Portfolio = () => {
+  const [filteredData, setFilteredData] = useState('All');
+  const [hoveredData, setHoveredData] = useState(null);
+
+  const handlerFilter = currentId => {
+    setFilteredData(currentId);
+  };
+
+  const handlerHover = index => {
+    setHoveredData(index);
+  };
+
+  const filteredItems =
+    filteredData === 'All'
+      ? portfolioData
+      : portfolioData.filter(item => item.tag === filteredData);
+
   return (
     <section id="portfolio" className="portfolio">
       <PageHeaderContent
         headerText="My Portfolio"
         icon={<BsBriefcaseFill size={40} />}
       />
+      <div className="portfolio__content">
+        <ul className="portfolio__content__filter">
+          {filterData.map(item => (
+            <li
+              className={item.label === filteredData ? 'active' : ''}
+              key={item.filterId}
+              onClick={() => handlerFilter(item.label)}
+            >
+              {item.label}
+            </li>
+          ))}
+        </ul>
+        <div className="portfolio__content__cards">
+          {filteredItems.map((item, index) => (
+            <div
+              key={item.id}
+              className="portfolio__content__cards__item"
+              onMouseEnter={() => handlerHover(index)}
+              onMouseLeave={() => handlerHover(null)}
+            >
+              <div className="portfolio__content__cards__item__img-wrapper">
+                <a href="#">
+                  <img src={item.image} alt={item.name} />
+                </a>
+              </div>
+              <div className="overlay">
+                {index === hoveredData && (
+                  <div className="overlay__content">
+                    <h2>{item.name}</h2>
+                    <p>{item.desctription}</p>
+                    <div className="overlay__content__buttons">
+                      <button>Code</button>
+                      <button>View</button>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
---
+++ src/utils/portfolioData.js
@@ -0,0 +1,73 @@
+export const portfolioData = [
+  {
+    id: 1,
+    name: 'Bookshelf',
+    tag: 'Design',
+    image: '../images/bookshelf_big_01-65a78bcf5f752.webp',
+    desctription:
+      'Team project. A website with a responsive layout, included auth, filters, add book to wishlist. Role: SCRUM Master, developer, header/modal/authorization. Stack: HTML, CSS, JavaScript, REST API, Firebase',
+  },
+  {
+    id: 2,
+    name: 'Finder',
+    tag: 'Development',
+    image: '../images/finder_big_01-65a78bd1dc09b.webp',
+    desctription:
+      'Individual project. A website with a responsive layout for search photo/video on Pixabay.com, included search via tags. Stack: HTML, CSS, JavaScript, REST API',
+  },
+  {
+    id: 3,
+    name: 'Messenger',
+    tag: 'Development',
+    image: '../images/firebase_messenger_big-65a78bd5e895d.webp',
+    desctription:
+      'Individual project. Chat app with user authorisation, user search, ability to send messages, photos. Stack: HTML/SASS, React, Firebase Auth/Firestore',
+  },
+  {
+    id: 4,
+    name: 'MovieDB',
+    tag: 'Development',
+    image: '../images/moviedb_big_01-65a78bd7e5db9.webp',
+    desctription:
+      'Individual project. Movies base app with recent movies, movie search, movie details, connected to themoviedb.org.  Stack: HTML, React, Styled Components, Axios, REST API',
+  },
+  {
+    id: 5,
+    name: 'Phonebook',
+    tag: 'Development',
+    image: '../images/phonebook_big-65a78bdbd66df.webp',
+    desctription:
+      'Individual project. Phonebook app with user authorisation, user filter, ability to add/remove user, connected to Swagger backend API. Stack: HTML, React, Redux, MUI.',
+  },
+  {
+    id: 6,
+    name: 'WebStudio',
+    tag: 'Design',
+    image: '../images/webstudio_big-65a78bdd64ac2.webp',
+    desctription:
+      'Individual project. A website with a responsive layout. Stack: HTML, CSS',
+  },
+  {
+    id: 7,
+    name: 'Yachts Adventure',
+    tag: 'Design',
+    image: '../images/yachts_big-65a78bdf8a7ec.webp',
+    desctription:
+      'Team project. A website with a pesponsive layout. Role: SCRUM Master, developer, gallery section/page animations. Stack: HTML, CSS, JavaScript, AOS, Swiper',
+  },
+];
+
+export const filterData = [
+  {
+    filterId: 1,
+    label: 'All',
+  },
+  {
+    filterId: 2,
+    label: 'Development',
+  },
+  {
+    filterId: 3,
+    label: 'Design',
+  },
+];
